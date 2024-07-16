6afa27458a772e84d9675de73077d0aaaa5fdfbb
PODARU Sebastian CONSULTANT EXTERN <Sebastian.Podaru@orange.com>
2024-07-16 18:05:25 +0300

    lesson 6 rustlings completed until strings

---
+++ lesson_6/rustlings/exercises/02_functions/functions1.rs
@@ -0,0 +1,8 @@
+// TODO: Add some function with the name `call_me` without arguments or a return value.
+fn call_me() {
+    // TODO: Print the string "Hello, world!"
+    println!("Hello, world!");
+}
+fn main() {
+    call_me(); // Don't change this line
+}
---
+++ lesson_6/rustlings/exercises/08_enums/enums1.rs
@@ -0,0 +1,17 @@
+#[derive(Debug)]
+enum Message {
+    // TODO: Define a few types of messages as used below.
+    Quit,
+    Echo,
+    Move,
+    ChangeColor,
+    Resize
+}
+
+fn main() {
+    println!("{:?}", Message::Resize);
+    println!("{:?}", Message::Move);
+    println!("{:?}", Message::Echo);
+    println!("{:?}", Message::ChangeColor);
+    println!("{:?}", Message::Quit);
+}
